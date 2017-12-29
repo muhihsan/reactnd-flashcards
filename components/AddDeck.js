127e4f4c6e3e4b753fa1d1a781ad1aa7c1021e88
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-30 01:29:55 +1100

    feat: Add new deck title to local storage and go back to home page

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Platform, StyleSheet } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import { saveDeckTitle } from '../utils/api';
 
 class AddDeck extends Component {
   state = {
     deckTitle: ''
+  }
+
+  toHome = () => {
+    this.props.navigation.dispatch(NavigationActions.back({
+      key: 'AddDeck'
+    }));
+  }
+
+  submit = () => {
+    const self = this;
+    const { deckTitle } = this.state;
+    saveDeckTitle(deckTitle).then(() => {
+      self.toHome();
+    });
   }
 
   render() {
@@ -11,7 +27,11 @@ class AddDeck extends Component {
       <View style={styles.item}>
         <Text>What is the title of your new deck?</Text>
         <TextInput style={styles.input} onChangeText={(deckTitle) => this.setState({ deckTitle })} />
-        <Text>Submit</Text>
+        <TouchableOpacity
+          style={Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn}
+          onPress={this.submit}>
+          <Text style={styles.submitBtnText}>SUBMIT</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -25,7 +45,31 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'gray',
     borderWidth: 1
-  }
+  },
+  iosSubmitBtn: {
+    backgroundColor: 'purple',
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: 'purple',
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    borderRadius: 2,
+    height: 45,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitBtnText: {
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'center'
+  },
 });
 
 export default AddDeck;
