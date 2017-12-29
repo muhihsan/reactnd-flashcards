1c7890adcab139171fa0a2de6b0b1ed8979f6f94
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-29 23:25:52 +1100

    feat: Show list of decks from local storage on the Decks component

--- components/Decks.js
+++ components/Decks.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { getDecks, addCardToDeck } from '../utils/api';
+import { createCard } from '../utils/helpers';
 
 class Decks extends Component {
+  state = {
+    ready: false,
+    decks: null
+  }
+
+  componentDidMount() {
+    var self = this;
+    addCardToDeck("React", createCard(
+      "Is this React Nanodegree worth it",
+      "Yes, totally!"
+    )).then(() => {
+      getDecks()
+        .then((decks) => {
+          self.setState({ decks });
+        });
+    });
+  }
+
   render() {
+    const { decks } = this.state;
+
     return (
       <View>
         <Text>Decks Component</Text>
+        <Text>{JSON.stringify(decks)}</Text>
       </View>
     );
   }
