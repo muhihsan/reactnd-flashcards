436d1b320765367174746b7fc5f5df8269b04423
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-29 15:47:27 +1100

    feat: Add NewDeck component and show it on the main screen along with Deck component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Decks from './components/Decks';
+import NewDeck from './components/NewDeck';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Decks />
+      <View>
+        <Decks />
+        <NewDeck />
+      </View>
     );
   }
 }
