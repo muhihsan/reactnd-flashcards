773b9745efa7a0419c99f327e4bd234ba631ea84
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-30 00:36:16 +1100

    feat: Show deck information on the DeckDetail component

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { AppLoading } from 'expo';
+import { getDeck } from '../utils/api';
 
 class DeckDetail extends Component {
+  state = {
+    ready: false,
+    deck: null
+  }
+
   static navigationOptions = ({ navigation }) => {
     const { deckTitle } = navigation.state.params;
-
     return {
       title: deckTitle
     };
   }
 
+  componentDidMount() {
+    const self = this;
+    const { deckTitle } = this.props.navigation.state.params;
+    getDeck(deckTitle)
+      .then((deck) => (
+        self.setState({
+          ready: true,
+          deck
+        }))
+      );
+  }
+
   render() {
+    const { ready, deck } = this.state;
+
+    if (!ready) {
+      return <AppLoading />
+    }
+
     return (
       <View>
-        <Text>Deck Detail Component</Text>
+        <Text>{deck.title}</Text>
+        <Text>{deck.questions.length} card{deck.questions.length > 1 ? 's' : ''}</Text>
+        <Text>Add Card</Text>
+        <Text>Start Quiz</Text>
       </View>
     );
   }
