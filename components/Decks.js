1d0df2088a1283623dadd715f85a8f1bb9e1db06
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-29 23:46:05 +1100

    feat: Show each of decks on its own UI on Decks component

--- components/Decks.js
+++ components/Decks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Platform } from 'react-native';
 import { getDecks, addCardToDeck } from '../utils/api';
 import { createCard } from '../utils/helpers';
 
@@ -11,8 +11,8 @@ class Decks extends Component {
 
   componentDidMount() {
     var self = this;
-    addCardToDeck("React", createCard(
-      "Is this React Nanodegree worth it",
+    addCardToDeck("JavaScript", createCard(
+      "Does React Nanodegree teach you JavaScript",
       "Yes, totally!"
     )).then(() => {
       getDecks()
@@ -24,14 +24,37 @@ class Decks extends Component {
 
   render() {
     const { decks } = this.state;
+    console.log(decks);
 
     return (
       <View>
-        <Text>Decks Component</Text>
-        <Text>{JSON.stringify(decks)}</Text>
+        {decks && decks.map((deck) => (
+          <View key={deck.title} style={styles.item}>
+            <Text>{JSON.stringify(deck)}</Text>
+          </View>
+        ))}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: 'white',
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  }
+});
 
 export default Decks;
