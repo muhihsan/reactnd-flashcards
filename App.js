df4eb8c8d4b0a00cfa3f4a5a84ae68bf7f66739b
Muhammad Ihsan <mihsan191@gmail.com>
2017-12-29 15:35:00 +1100

    feat: Add api.js that manages AsyncStorage database

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getDeck, getDecks, addCardToDeck, saveDeckTitle, clear } from './utils/api';
 
 export default class App extends React.Component {
   render() {
@@ -7,7 +8,6 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
---
+++ utils/api.js
@@ -0,0 +1,48 @@
+import { AsyncStorage } from 'react-native';
+
+const FLASHCARDS_STORAGE_KEY = 'FlashCards:decks'
+
+export function getDecks() {
+  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+    .then((result) => JSON.parse(result));
+}
+
+export function getDeck(title) {
+  return getDecks().then((decks) => {
+    return (decks && decks[title]) ? decks[title] : null;
+  });
+}
+
+export function saveDeckTitle(title) {
+  getDeck(title).then((deck) => {
+    if (!deck) {
+      return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+        [title]: {
+          title: title,
+          questions: []
+        }
+      }));
+    }
+  });
+}
+
+export function addCardToDeck(title, card) {
+  return getDeck(title).then((deck) => {
+    if (deck) {
+      return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+        [deck.title]: {
+          title: deck.title,
+          questions: [...deck.questions, card]
+        }
+      }));
+    }
+    else {
+      return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+        [title]: {
+          title: title,
+          questions: [card]
+        }
+      }));
+    }
+  });
+}
