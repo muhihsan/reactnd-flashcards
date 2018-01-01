c79012ed95778a9bc9d583e0720b93cf76ddebdd
Muhammad Ihsan <mihsan191@gmail.com>
2018-01-01 17:50:53 +1100

    feat: Get deck detail from redux store

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 import { AppLoading } from 'expo';
 import { getDeck } from '../utils/api';
 
 class DeckDetail extends Component {
-  state = {
-    ready: false,
-    deck: null
-  }
-
   static navigationOptions = ({ navigation }) => {
     const { deckTitle } = navigation.state.params;
     return {
@@ -16,40 +12,33 @@ class DeckDetail extends Component {
     };
   }
 
-  componentDidMount() {
-    const self = this;
-    const { deckTitle } = this.props.navigation.state.params;
-    getDeck(deckTitle)
-      .then((deck) => (
-        self.setState({
-          ready: true,
-          deck
-        }))
-      );
-  }
-
   render() {
-    const { ready, deck } = this.state;
-
-    if (!ready) {
-      return <AppLoading />
-    }
+    const { deck } = this.props;
 
     return (
       <View>
         <Text>{deck.title}</Text>
         <Text>{deck.questions.length} card{deck.questions.length > 1 ? 's' : ''}</Text>
         <TouchableOpacity
-            onPress={() => this.props.navigation.navigate(
-              'AddCard',
-              { deckTitle: deck.title }
-            )}>
-            <Text>Add Card</Text>
-          </TouchableOpacity>
+          onPress={() => this.props.navigation.navigate(
+            'AddCard',
+            { deckTitle: deck.title }
+          )}>
+          <Text>Add Card</Text>
+        </TouchableOpacity>
         <Text>Start Quiz</Text>
       </View>
     );
   }
 }
 
-export default DeckDetail;
+function mapStateToProps(state, { navigation }) {
+  const { deckTitle } = navigation.state.params;
+
+  return {
+    deckTitle,
+    deck: state.decks.find((deck) => deck.title === deckTitle)
+  };
+}
+
+export default connect(mapStateToProps)(DeckDetail);
